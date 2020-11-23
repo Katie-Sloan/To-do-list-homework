3eaf8220c92a8ca8b96b6479cc3c323be6a330ca
Katie Sloan <katie.a.sloan@gmail.com>
2020-11-23 16:15:25 +0000

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,42 @@
 <template>
   <div id="app">
-    <form v-on:submit.prevent="saveNewItem"></form>
-      <label for="new-item">Add a new item: </label>
+    <form v-on:submit.prevent="saveNewItem">
+      <label for="new-item">ToDo's:</label>
       <input id="new-item" type="text" v-model="newItem"/>
-      <input type="submit" value="Save new item"/>
+      <input type="submit" value="Save item"/>
     </form>
 
     <ul>
-      <li v-for="(item.index) in items" 
-      :key="index"
-      :class="item.isHighPriority ? 'high-priority':'low-priority'"
+      <li v-for="(item, index) in items" :key="index" :class="item.isHighPriority ? 'high-priority':'low-priority'">
         <span>{{item.name}}</span>
       </li>
     </ul>
   </div>
 </template>
+
+<script>
+export default {
+  data(){
+    return {
+      items: [
+        {name: "Buy shopping", isHighPriority: true},
+        {name: "Clean bathroom", isHighPriority: false},
+        {name: "Car's MOT", isHighPriority: true}
+      ],
+      newItem: ""
+    }
+  },
+  methods: {
+    saveNewItem: function(){
+      this.items.push({
+        name: this.newItem,
+        isHighPriority: false
+      });
+      this.newItem = "";
+    }
+  }  
+}
+</script>
 
 <style>
 li.high-priority {
